465f352072c4b8826dbffd114314285f7f9cf6e8
Lennart Alvestad <alvestad@gmail.com>
2018-07-02 17:43:40 +0200

    refactoring test: remove duplications

--- hub/src/test/java/ch/prevo/open/hub/match/MatcherTest.java
+++ hub/src/test/java/ch/prevo/open/hub/match/MatcherTest.java
@@ -3,6 +3,7 @@ package ch.prevo.open.hub.match;
 import ch.prevo.open.encrypted.model.InsurantInformation;
 import org.junit.Test;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -12,19 +13,22 @@ import static org.junit.Assert.*;
 
 public class MatcherTest {
 
+    private static final String OASI_1 = "oasi1";
+    private static final String UID_1 = "uid1";
+    private static final String OASI_2 = "oasi2";
+    private static final String UID_2 = "uid2";
+    private static final String UID_3 = "uid3";
+
     @Test
     public void findMatches() throws Exception {
-        Set<InsurantInformation> exits = new HashSet<>();
-        exits.add(new InsurantInformation("oasi1", "uid1"));
-        Set<InsurantInformation> entries = new HashSet<>();
-        entries.add(new InsurantInformation("oasi1", "uid2"));
-        entries.add(new InsurantInformation("oasi2", "uid3"));
+        Set<InsurantInformation> exits = createSet(new InsurantInformation(OASI_1, UID_1));
+        Set<InsurantInformation> entries = createSet(new InsurantInformation(OASI_1, UID_2), new InsurantInformation(OASI_2, UID_3));
 
         List<Match> matches = new MatcherService().findMatches(exits, entries);
 
         assertEquals(1, matches.size());
         Match match = matches.get(0);
-        assertEquals("oasi1", match.getEncryptedOasiNumber());
+        assertEquals(OASI_1, match.getEncryptedOasiNumber());
         assertNotEquals(match.getPreviousRetirementFundUid(), match.getNewRetirementFundUid());
     }
 
@@ -35,12 +39,15 @@ public class MatcherTest {
 
     @Test(expected = RuntimeException.class)
     public void findMatchesWithDuplicates() throws Exception {
-        Set<InsurantInformation> exits = new HashSet<>();
-        exits.add(new InsurantInformation("oasi1", "uid1"));
-        Set<InsurantInformation> entries = new HashSet<>();
-        entries.add(new InsurantInformation("oasi1", "uid2"));
-        entries.add(new InsurantInformation("oasi1", "uid3"));
+        Set<InsurantInformation> exits = createSet(new InsurantInformation(OASI_1, UID_1));
+        Set<InsurantInformation> entries = createSet(new InsurantInformation(OASI_1, UID_2), new InsurantInformation(OASI_1, UID_3));
 
         new MatcherService().findMatches(exits, entries);
+    }
+
+    private Set<InsurantInformation> createSet(InsurantInformation... insurantInformations) {
+        Set<InsurantInformation> result = new HashSet<>();
+        Collections.addAll(result, insurantInformations);
+        return result;
     }
 }
