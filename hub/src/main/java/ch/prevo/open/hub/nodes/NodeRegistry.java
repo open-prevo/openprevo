d622ba0f622acdfb3d61f67bfbea90cb45ad8abd
Michael Heinrichs <netopyr@users.noreply.github.com>
2018-07-04 15:30:43 +0200

    Configurable place for config-file

--- hub/src/main/java/ch/prevo/open/hub/nodes/NodeRegistry.java
+++ hub/src/main/java/ch/prevo/open/hub/nodes/NodeRegistry.java
@@ -4,15 +4,15 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.List;
 
@@ -24,22 +24,27 @@ class NodeRegistry {
 
     private static Logger LOG = LoggerFactory.getLogger(NodeRegistry.class);
 
+    private final ResourceLoader loader;
     private final ObjectMapper mapper;
+
+    @Value("${open.prevo.hub.config.file}")
+    private String configFile;
 
     private List<NodeConfiguration> nodes = Collections.emptyList();
 
     @Inject
-    public NodeRegistry(@Named("yamlMapper") ObjectMapper mapper) {
+    public NodeRegistry(ResourceLoader loader, @Named("yamlMapper") ObjectMapper mapper) {
+        this.loader = loader;
         this.mapper = mapper;
     }
 
     @PostConstruct
     public void init() {
-        try (final InputStream yaml = NodeRegistry.class.getResourceAsStream("nodes.yaml")) {
-            final BufferedReader reader = new BufferedReader(new InputStreamReader(yaml, "UTF-8"));
-            nodes = mapper.readValue(reader, new TypeReference<List<NodeConfiguration>>() {});
+        try {
+            final Resource resource = loader.getResource(configFile);
+            nodes = mapper.readValue(resource.getInputStream(), new TypeReference<List<NodeConfiguration>>() {});
         } catch (IOException e) {
-            LOG.warn("Unable to read bootstrap-data from nodes.yaml", e);
+            LOG.warn("Unable to read bootstrap-data from " + configFile, e);
         }
     }
 
