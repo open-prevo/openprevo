c84a83201632bbf356b83d6b69ed93f0aa683eaa
Lennart Alvestad <alvestad@gmail.com>
2018-07-05 17:31:32 +0200

    add initial MatchNotificationProvider interface
    Note: not next to the JobStartProvider/JobEndProvider interfaces,
    assume those will be moved to the data-model module anyway (and now
    the excel adapter doesn't need this dependency)

---
+++ data-model/src/main/java/ch/prevo/open/data/provider/MatchNotificationProvider.java
@@ -0,0 +1,7 @@
+package ch.prevo.open.data.provider;
+
+public interface MatchNotificationProvider {
+
+    void notifyEmploymentCommencementMatch();
+    void notifyEmploymentTerminationsMatch();
+}
