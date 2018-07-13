dff9783685231d58f10a417944e857ccacba46f4
Michael Heinrichs <netopyr@users.noreply.github.com>
2018-07-13 14:34:59 +0200

    Added tests and extended existing once to check the possibility to use multiple retirement funds within a single node.

--- hub/src/main/java/ch/prevo/open/hub/nodes/DockerNodeRegistry.java
+++ hub/src/main/java/ch/prevo/open/hub/nodes/DockerNodeRegistry.java
@@ -1,12 +1,10 @@
 package ch.prevo.open.hub.nodes;
 
-import static java.util.Collections.singletonList;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.Arrays;
 import java.util.List;
-
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
 /**
  * All node instances currently known by the hub, currently only a mock implementation.
@@ -17,10 +15,10 @@ class DockerNodeRegistry implements NodeRegistry {
 
     static final NodeConfiguration BALOISE_NODE =
             new NodeConfiguration("http://node_baloise:8080",
-                    singletonList("CHE-109.740.084-Baloise-Sammelstiftung"));
+                    "CHE-109.740.084-Baloise-Sammelstiftung", "CHE-109.740.084-Baloise-Sammelstiftung 2");
     static final NodeConfiguration HELVETIA_NODE =
             new NodeConfiguration("http://node_helvetia:8080",
-                    singletonList("CHE-109.537.488-Helvetia-Prisma-Sammelstiftung"));
+                    "CHE-109.537.488-Helvetia-Prisma-Sammelstiftung", "CHE-109.537.488-Helvetia-Prisma-Sammelstiftung 2");
 
     @Override
     public List<NodeConfiguration> getCurrentNodes() {
