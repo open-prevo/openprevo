ce936b57985d480ac4f54f3c15f6991882012a27
Michael Heinrichs <netopyr@users.noreply.github.com>
2018-07-20 10:12:50 +0200

    Added toString() method which I forgot

--- encrypted-data-model/src/main/java/ch/prevo/open/encrypted/model/MatchForTermination.java
+++ encrypted-data-model/src/main/java/ch/prevo/open/encrypted/model/MatchForTermination.java
@@ -2,6 +2,8 @@ package ch.prevo.open.encrypted.model;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 
 import java.time.LocalDate;
 
@@ -29,18 +31,6 @@ public class MatchForTermination extends MatchNotification {
         this.transferInformation = transferInformation;
     }
 
-    /**
-     * I.E. are all attributes except the encrypted transfer info equal
-     */
-    public boolean isSameMatch(MatchForTermination o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        return new EqualsBuilder()
-                .appendSuper(super.equals(o))
-                .isEquals();    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -61,5 +51,13 @@ public class MatchForTermination extends MatchNotification {
                 .appendSuper(super.hashCode())
                 .append(transferInformation)
                 .toHashCode();
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.SHORT_PREFIX_STYLE)
+                .appendSuper(super.toString())
+                .append("transferInformation", transferInformation)
+                .toString();
     }
 }
