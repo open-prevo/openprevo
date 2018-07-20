4596eab5c9a9c128a4838e5db0cd27284dc92d35
mikaelkalt <4425489+mikaelkalt@users.noreply.github.com>
2018-07-20 11:48:06 +0200

    Add missing database type configuartion

--- adapter-example-spring/src/main/java/org/example/prevo/open/adapter/AdapterConfig.java
+++ adapter-example-spring/src/main/java/org/example/prevo/open/adapter/AdapterConfig.java
@@ -13,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
 @Configuration
@@ -35,6 +36,7 @@ public class AdapterConfig {
 
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setDatabase(Database.H2);
 
         LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
         factory.setJpaVendorAdapter(vendorAdapter);
