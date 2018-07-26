e574a5faf1c850ed62ab21ae7f0930944a9f7798
mikaelkalt <4425489+mikaelkalt@users.noreply.github.com>
2018-07-26 15:55:39 +0200

    Allow execution of all nodes at once on Mac

--- demo/src/startExcelDemo.sh
+++ demo/src/startExcelDemo.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 doStart() {
     if hash start 2>/dev/null; then
 		echo on windows please use the bat
@@ -5,11 +6,15 @@ doStart() {
 		cd $1
 		gnome-terminal -t $1 -- ./start.sh
 		cd ..
+    elif hash open; then
+        cd $1
+        open -a Terminal.app start.sh
+        cd ..
 	else
 		echo  termnial programm not found. exiting
     fi
 }
-for folder in NodeBaloiseExcel NodeHelvetiaExcel NodeZurichExcel hub  
+for folder in NodeBaloiseExcel NodeHelvetiaExcel NodeZurichExcel Hub
 do  
     doStart $folder
 done  
